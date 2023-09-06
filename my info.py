b4a9782be6bde6e02605f6fe6542efbb6f89dd05
varunmuniganti80 <144286390+varunmuniganti80@users.noreply.github.com>
2023-09-06 18:33:56 -0400

    my information v1

---
+++ my info.py
@@ -0,0 +1,7 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/varun/AppData/Local/Programs/Python/Python311/my info.py
+hi my name is varun sir ram muniganti
+i saty at ecil x roads, hyderabad,india 500062
+my contact number is 000000000
